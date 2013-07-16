dbf95e8bba132f730c4c7042cd8d3a4863e90531
IgorDePaula <principe.borodin@gmail.com>
2013-07-16 14:01:48 -0300

    conserto de rota delete

--- src/Helpdesk/Controller/CategoriaChamadoController.php
+++ src/Helpdesk/Controller/CategoriaChamadoController.php
@@ -122,7 +122,7 @@ class CategoriaChamadoController extends AbstractActionController {
         }
 
 
-        return array('form' => $form, 'setor' => $setorid);
+        return array('form' => $form, 'setor' => $setor,'setorid'=>$setorid);
     }
 
 }
