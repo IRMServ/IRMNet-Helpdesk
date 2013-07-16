18b2113fe2691efc8aa0683922862a819e2bdcac
IgorDePaula <principe.borodin@gmail.com>
2013-07-16 14:14:45 -0300

    Inserido assetmanager

--- config/module.config.php
+++ config/module.config.php
@@ -664,4 +664,11 @@ return array(
             },
         )
     ),
+                     'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public',
+            ),
+        ),
+    ),
 );
